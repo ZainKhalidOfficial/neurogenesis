dfa6958062926b84a3a4ee1fc692078165d0f6de
ZainKhalidOfficial <thezainkhalidofficial@gmail.com>
2025-06-11 18:00:33 +0500

    File Formatting - Base KNN - Barebone Format

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ y = data[:, -1]   # Target variable
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
 
 
-from knn import KNN
+from models.knn import KNN
 clf = KNN(k=3)
 clf.fit(X_train, y_train)
 predictions = clf.predict(X_test)
