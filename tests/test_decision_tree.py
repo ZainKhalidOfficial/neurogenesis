6ea50d0d576cebca4ce6ef4f08405fca42196c05
ZainKhalidOfficial <thezainkhalidofficial@gmail.com>
2025-07-16 17:29:12 +0500

    Decision Tree - Barebone Format

---
+++ tests/test_decision_tree.py
@@ -0,0 +1,30 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '')))
+
+
+from models.decision_tree import DecisionTree
+
+def accuracy(y_true, y_pred):
+    accuracy = np.sum(y_true == y_pred) / len(y_true)
+    return accuracy
+
+data = datasets.load_breast_cancer()
+
+X = data.data
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+clf = DecisionTree(max_depth=10)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+acc = accuracy(y_test, y_pred)
+
+print("Accuracy:", acc)
+
